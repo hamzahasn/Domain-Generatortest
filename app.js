d18d411a6e2470b0c1d04a792e787099d828f234
hamzahasn <hamza.hasnain@gmail.com>
2020-10-27 00:40:53 +0000

    domain generator

---
+++ app.js
@@ -0,0 +1,11 @@
+ var pronoun = ['the','our'];
+  var adj = ['scary', 'terrifying', 'horrific', 'frightening', 'spooky'];
+  var noun = ['dracula','frankenstein', 'werewolf', 'ghost', 'mummy' ];
+ 
+ for(var i = 0; i<pronoun.length;i++)
+ for(var j = 0; j<adj.length;j++)
+ for(var k = 0; k<noun.length;k++)
+  console.log(pronoun[i] + adj[i] + noun[i]+'.com'); 
+
+
+
